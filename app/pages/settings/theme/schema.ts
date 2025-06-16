37fda3e0f75f61cdefb046538d3de7a45c4fce22
danbiilee <danbi.db@gmail.com>
2025-06-17 01:18:36 +0900

    feat: monthly_themes.theme -> title 컬럼명 변경

--- app/pages/settings/theme/schema.ts
+++ app/pages/settings/theme/schema.ts
@@ -1,13 +1,13 @@
 import { bigint, text, timestamp, pgTable, uuid } from "drizzle-orm/pg-core";
 import { profiles } from "../profile/schema";
 
-// TODO: 작성자 아이디 컬럼 확인
+// TODO: 한달에 한개만 작성 가능하도록 제약 추가
 export const monthly_themes = pgTable("monthly_themes", {
   theme_id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
   profile_id: uuid()
     .notNull()
     .references(() => profiles.profile_id, { onDelete: "cascade" }),
-  theme: text().notNull(),
+  title: text().notNull(),
   description: text(),
   image: text(),
   created_at: timestamp().notNull().defaultNow(),
